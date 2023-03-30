81c30118f2d0d291e3da124cb5c3c750a4c03e8c
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-03-30 11:45:04 +0200

    Complete and clarify texts

--- src/components/PleaseSelect.js
+++ src/components/PleaseSelect.js
@@ -18,7 +18,9 @@ function PleaseSelect(props) {
                         This annotation belongs to the PDF file. If you
                         wish to convert it to a LIDIA annotation, first
                         import it into Zotero using <strong>File -&gt;
-                        Import Annotations. (If this</strong>
+                        Import Annotations</strong>. (If this PDF file is part
+                        of a group library, you may first have to move it to
+                        a personal library.)
                     </p>
                 </div>
             }
@@ -36,6 +38,15 @@ function PleaseSelect(props) {
                     <p>
                         The annotation you selected is not a LIDIA annotation.
                         Please select a LIDIA annotation or create a new one.
+                        {props.convertible &&
+                            <span> Alternatively, you may try to convert it to
+                            a LIDIA annotation using the button below.</span>
+                        }
+                        {!props.convertible &&
+                            <span> Conversion to a LIDIA annotation is not
+                            available because this annotation cannot be edited
+                            by you.</span>
+                        }
                     </p>
                     {props.convertible &&
                         <div><button onClick={handleConvert}>Convert to LIDIA annotation</button></div>
