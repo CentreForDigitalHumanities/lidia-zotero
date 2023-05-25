444e24e4ade5df4374efaa7c29a23a71e8160654
Arjan Mossel <arjanmossel@gmail.com>
2023-05-25 10:02:17 +0200

    Update metadata

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 rm -rf build
 mkdir build
-zip -r build/lidia-annotations_v0.1.0.xpi *
+zip -r build/lidia-annotations-v0.2.0.xpi *
 cd build
-jq ".addons[\"lidia-annotations@dig.hum.uu.nl\"].updates[0].update_hash = \"sha256:`shasum -a 256 lidia-annotations_v0.1.0.xpi | cut -d' ' -f1`\"" ../updates.json.tmpl > updates.json
+jq ".addons[\"lidia-annotations@cdh.uu.nl\"].updates[0].update_hash = \"sha256:`shasum -a 256 lidia-annotations-v0.2.0.xpi | cut -d' ' -f1`\"" ../updates.json.tmpl > updates.json
