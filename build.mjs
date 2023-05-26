eff38b38790ea9332802045ad28fd778e0480631
Arjan Mossel <arjanmossel@gmail.com>
2023-05-26 11:18:13 +0200

    Revert "Move plugin code to plugin dir for zotero-plugin-zipup postbuild"
    This reverts commit 0967e1924aa18868fefb6377c1f757f7c567300a.

--- build.mjs
+++ build.mjs
@@ -3,13 +3,12 @@ import 'zotero-plugin/copy-assets.js';
 import 'zotero-plugin/rdf.js';
 import 'zotero-plugin/version.js';
 
-
 async function build() {
     await esbuild.build({
-        entryPoints: ['plugin/src/lib.js'],
+        entryPoints: ['src/lib.js'],
         bundle: true,
         loader: { '.js': 'jsx' },
-        outfile: 'plugin/out.js',
+        outfile: 'out.js',
     })
 }
 
