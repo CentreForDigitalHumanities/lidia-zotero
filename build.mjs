f025b38182cfe2ed2cca60b6f7f088062e6e6843
Arjan Mossel <arjanmossel@gmail.com>
2023-05-26 11:07:21 +0200

    Generate install.rdf and update.rdf

--- build.mjs
+++ build.mjs
@@ -1,8 +1,19 @@
 import * as esbuild from 'esbuild';
+import 'zotero-plugin/copy-assets.js';
+import 'zotero-plugin/rdf.js';
+import 'zotero-plugin/version.js';
 
-await esbuild.build({
-    entryPoints: ['plugin/src/lib.js'],
-    bundle: true,
-    loader: { '.js': 'jsx' },
-    outfile: 'plugin/out.js',
-});
+
+async function build() {
+    await esbuild.build({
+        entryPoints: ['plugin/src/lib.js'],
+        bundle: true,
+        loader: { '.js': 'jsx' },
+        outfile: 'plugin/out.js',
+    })
+}
+
+build().catch(err => {
+    console.log(err)
+    process.exit(1)
+})
