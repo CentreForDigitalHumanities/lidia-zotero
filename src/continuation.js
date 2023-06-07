38778264ce6230fb3aab0093afcc0e3e79d2c63b
Arjan Mossel <arjanmossel@gmail.com>
2023-06-07 17:05:18 +0200

    Try checking for isFileAttachment
    Cannot reproduce the issue locally

--- src/continuation.js
+++ src/continuation.js
@@ -11,40 +11,45 @@ import { deserialize } from "./serialize.js";
  */
 export function getPreviousAnnotation(item) {
     // Get all annotations in current document
-    const pdfItem = item.parentItem;
-    const annotations = pdfItem.getAnnotations(includeTrashed=false);
-    /* Sort annotations according to annotationSortIndex
-     * (It seems that we already get the annotations in the right order,
-     * but not sure if this is guaranteed) */
-    annotations.sort((x, y) => x.annotationSortIndex - y.annotationSortIndex);
-    let currentIndex = 0;
-    let found = false;
-    for (; currentIndex < annotations.length; currentIndex++) {
-        const currentItem = annotations[currentIndex];
-        if (currentItem.key === item.key) {
-            found = true;
-            break;
+    if (item.parentItem.isFileAttachment()) {
+        const pdfItem = item.parentItem;
+        const annotations = pdfItem.getAnnotations(includeTrashed=false);
+        /* Sort annotations according to annotationSortIndex
+        * (It seems that we already get the annotations in the right order,
+        * but not sure if this is guaranteed) */
+        annotations.sort((x, y) => x.annotationSortIndex - y.annotationSortIndex);
+        let currentIndex = 0;
+        let found = false;
+        for (; currentIndex < annotations.length; currentIndex++) {
+            const currentItem = annotations[currentIndex];
+            if (currentItem.key === item.key) {
+                found = true;
+                break;
+            }
         }
-    }
-    if (!found || currentIndex === 0) {
-        /* Return undefined if the current annotation cannot be found
-         * (which is not supposed to happen) or if it has 0 as its index,
-         * which means that there is no previous annotation. */
-        return null;
-    }
-    let previousIndex = currentIndex - 1;
-    found = false;
-    for (; previousIndex >= 0; previousIndex--) {
-        const previousItem = annotations[previousIndex];
-        const data = deserialize(previousItem.annotationComment);
-        if (data && !data.argcont) {
-            found = true;
-            break;
+        if (!found || currentIndex === 0) {
+            /* Return undefined if the current annotation cannot be found
+            * (which is not supposed to happen) or if it has 0 as its index,
+            * which means that there is no previous annotation. */
+            return null;
         }
-    }
-    if (found) {
-        return annotations[previousIndex];
+        let previousIndex = currentIndex - 1;
+        found = false;
+        for (; previousIndex >= 0; previousIndex--) {
+            const previousItem = annotations[previousIndex];
+            const data = deserialize(previousItem.annotationComment);
+            if (data && !data.argcont) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            return annotations[previousIndex];
+        } else {
+            return null;
+        }
     } else {
+        log(JSON.stringify(item.parentItem))
         return null;
     }
 }
