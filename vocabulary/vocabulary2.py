747d6933c42ea5916001cc035d1a8ea6d3936792
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-07-26 17:06:30 +0200

    Add new vocabulary convert script

---
+++ vocabulary/vocabulary2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+import openpyxl
+import json
+from pathlib import Path
+
+PROJROOT = Path(__file__).parents[1].resolve()
+
+
+def get_rowdicts(worksheet: openpyxl.worksheet.worksheet.Worksheet):
+    '''Use the first row for headers and return a list of dicts for every
+    following row'''
+    headers = []
+    rowdicts = []
+    for i, row in enumerate(worksheet.values):
+        if i == 0:
+            headers = row
+        else:
+            rowdict = {
+                header: row[headerindex]
+                for headerindex, header
+                in enumerate(headers)
+            }
+            rowdicts.append(rowdict)
+    return rowdicts
+
+
+class LexiconProcesser():
+    def process_lexicon(self, filepath: Path):
+        wb = openpyxl.load_workbook(filename=str(filepath))
+        # First read the categories
+        category_ws = wb['categories']
+        category_rowdicts = get_rowdicts(category_ws)
+        self.categories = {
+            row['slug']: row['display'] for row in category_rowdicts
+        }
+        # Now create a dictionary with the slugs of the items without
+        # parents as keys and lists of itself and its child items as values
+        entry_ws = wb['entries']
+        entry_rowdicts = get_rowdicts(entry_ws)
+        entries_hierarchical: dict[str, list[dict]] = {
+            x['slug']: [x] for x in entry_rowdicts if not x['parent']
+        }
+        entries_with_parent = [
+            x for x in entry_rowdicts if x['parent']
+        ]
+        for entry in entries_with_parent:
+            try:
+                entries_hierarchical[entry['parent']].append(entry)
+            except KeyError:
+                print(
+                    "Parent '{}' for '{}' not found. Skipping."
+                    .format(entry['parent'], entry['slug'])
+                )
+        # Now compile the full sorted list
+        self.entries = []
+        for headslug in entries_hierarchical:
+            entries_for_head = entries_hierarchical[headslug]
+            # Add indentation to all subentries and apply selectable default
+            # value (i.e. True)
+            for entry in entries_for_head:
+                if entry['parent']:
+                    entry['display'] = '- ' + entry['display']
+                if entry['selectable'] is None or entry['selectable'] == '':
+                    entry['selectable'] = True
+            # Sort alphabetically
+            # (this will cause the head entry to be put at the end, because
+            # the other entries are preceded by a dash)
+            entries_for_head.sort(key=lambda x: x['display'])
+            # Move the head entry back to front
+            head_entry = entries_for_head.pop(len(entries_for_head) - 1)
+            entries_for_head.insert(0, head_entry)
+            self.entries.extend(entries_for_head)
+
+    def write_json(self, filepath: Path):
+        data = {
+            'categories': self.categories,
+            'entries': self.entries
+        }
+        filepath.parent.mkdir(exist_ok=True, parents=True)
+        with filepath.open('w') as f:
+            json.dump(data, f, indent=2)
+
+
+if __name__ == '__main__':
+    processer = LexiconProcesser()
+    processer.process_lexicon(PROJROOT / 'lexicon.xlsx')
+    processer.write_json(PROJROOT / 'content' / 'lexicon.json')
